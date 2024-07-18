8f56ddff33c8762823d994c23f721266abaa56dc
ArgeisGutierrez <argenis.v.gtz@gmail.com>
2024-07-17 23:29:59 -0600

    fix problems with build
    se solucionaron 3 errores para crear el build

--- src/components/PatientDitail.tsx
+++ src/components/PatientDitail.tsx
@@ -26,12 +26,12 @@ export function PatientDetail({ patient }: PatientDetailProps) {
       <div className="flex justify-between gap-1 mt-10 flex-col lg:flex-row">
         <button
           className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold rounded-lg"
-          onClick={() => getPatientById()}>
+          onClick={() => getPatientById(patient.id)}>
           Editar
         </button>
         <button
           className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold rounded-lg"
-          onClick={() => handleDelete(patient.id)}>
+          onClick={() => handleDelete()}>
           Eliminar
         </button>
       </div>
